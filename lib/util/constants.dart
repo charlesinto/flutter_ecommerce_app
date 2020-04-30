4abe908e66ecc7c1874017a9e0205dc6e9c44ede
charlesinto <charles.onuorah@yahoo.com>
2020-04-30 01:17:21 +0100

    remove constants.dart

--- lib/util/constants.dart
+++
@@ -1,5 +0,0 @@
-String appDomain = 'http://167.99.154.149:1337';
-
-String pStackPublicKey = 'pk_test_665eb90f300a40ab558f49e21df7c334b51f675b';
-
-String pStackTest = 'sk_test_8ed095a4ef67cb0a8d6c3c540812dbe930d9764c';
